2ef4d5db790ada358e0ef84243dfd5801e34e297
so0112 <tkddh0112@gmail.com>
2023-01-11 18:05:05 +0900

    Feat: project img, description, github link 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { Inter } from '@next/font/google';
-import Layout from '../components/Layout';
+import Layout from '../components/home/Layout';
 import Link from 'next/link';
 
 export default function Home() {
