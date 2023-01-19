ba111c434aa658a79b04436bf314582a66ab1546
so0112 <tkddh0112@gmail.com>
2023-01-19 23:39:14 +0900

    Feat: 기술 스택 tags 추가

--- components/projects/ProjectItem.tsx
+++ components/projects/ProjectItem.tsx
@@ -13,29 +13,25 @@ const ProjectItem = ({ data }: ProjectItemProps) => {
   const imgSrc = data.cover?.external.url || "/images/nextjs.png";
   const tags = data.properties.Tags.multi_select;
 
-  console.log(tags);
-
   return (
     <div className="flex flex-col m-3 bg-slate-400 rounded-xl">
       <div>
-        <Image
-          className="rounded-t-xl"
-          src={imgSrc}
-          alt="cover image"
-          width="100"
-          height="60"
-          layout="responsive"
-          objectFit="none"
-        />
+        <Image className="rounded-t-xl" src={imgSrc} alt="cover image" width="50" height="60" layout="responsive" />
       </div>
 
       <div className="p-3">
-        <h1>{title}</h1>
+        <h1 className="font-bold text-xl">{title}</h1>
         <div>{description}</div>
-        <a className="hover:text-gray-300" href={github} target="_blank" rel="noreferrer">
+        <a className="hover:text-gray-400" href={github} target="_blank" rel="noreferrer">
           깃허브 바로가기
         </a>
-        <div className="flexe items-start mt-2"></div>
+        <div className="flex flex-wrap items-start mt-2">
+          {tags?.map((tag) => (
+            <h1 className="px-2 py-1 mr-2 mb-2 rounded-md bg-slate-500 text-xs" key={tag?.id}>
+              {tag?.name}
+            </h1>
+          ))}
+        </div>
       </div>
     </div>
   );
