548ece97d51f2139a9b939bc2213790dea1de56b
so0112 <tkddh0112@gmail.com>
2023-01-25 21:26:37 +0900

    Fix: animation module error fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import Layout from "../components/home/Layout";
-import Link from "next/link";
 import Intro from "../components/intro";
-import Animation from "../components/home/animation";
 
 export default function Home() {
   return (
