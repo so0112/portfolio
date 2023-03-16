d010deb5b95ba50905b2a15735647d97cc654f3f
so0112 <tkddh0112@gmail.com>
2023-03-16 18:53:31 +0900

    Fix: img 오류 해결

--- components/projects/ProjectItem.tsx
+++ components/projects/ProjectItem.tsx
@@ -12,15 +12,17 @@ const ProjectItem = ({ data }: ProjectItemProps) => {
   const description = data.properties.Description.rich_text[0].plain_text;
 
   // TODO: 이미지 data 에러 블로깅
-  const imgSrc = data.cover.file?.url || data.cover.external?.url;
+  const imgSrc = data.cover?.file?.url || data.cover?.external?.url;
 
   const tags = data.properties.Tags.multi_select;
 
   return (
-    <div className="flex flex-col m-3 rounded-xl">
-      <Image className="rounded-t-xl" src={imgSrc} alt="cover image" width="40" height="30" layout="responsive" />
+    <div className="flex flex-col m-3 rounded-xl w-67">
+      <div className="flex h-80 overflow-hidden">
+        <Image className="rounded-t-xl" src={imgSrc} alt="커버 이미지" width={1000} height={200} />
+      </div>
 
-      <div className="p-3 h-50 bg-slate-300 rounded-b-xl">
+      <div className="p-3 h-50 bg-slate-300 rounded-b-xl w-">
         <h1 className="font-bold text-xl">{title}</h1>
         <div>{description}</div>
         <a className="hover:text-gray-100" href={github} target="_blank" rel="noreferrer">
