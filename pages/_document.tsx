7327e64cafbe0fb56bbc3c3dd3cb4ae08ce71d6e
so0112 <tkddh0112@gmail.com>
2023-03-19 00:06:31 +0900

    Docs: title 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>FE 개발자 신상오</title>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,6 @@ export default function Home() {
   return (
     <>
       <Layout>
-        <Head>
-          <title>FE 개발자 신상오 포트폴리오</title>
-          <meta name="description" content="FE 개발자 신상오의 포트폴리오입니다." />
-        </Head>
         <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <Intro />
