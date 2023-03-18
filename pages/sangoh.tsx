a2b60603a37baea36479ad414bb5fbb25f9788ed
so0112 <tkddh0112@gmail.com>
2023-03-18 23:48:31 +0900

    Reanme: 파일명 변경

---
+++ pages/sangoh.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const sangoh = () => {
+  return <div>sangoh</div>;
+};
+
+export default sangoh;
--- pages/soshin.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const soshin = () => {
-  return <div>soshin</div>;
-};
-
-export default soshin;
